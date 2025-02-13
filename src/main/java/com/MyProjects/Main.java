978eb7b45db4b8d042ad97fd4a40276f8db0d395
Utkarsh Shukla <utkarshs9727@gmail.com>
2025-02-13 23:59:05 +0530

    First Commit

---
+++ src/main/java/com/MyProjects/Main.java
@@ -0,0 +1,7 @@
+package com.MyProjects;
+
+public class Main {
+    public static void main(String[] args) {
+          Server s = new Server();
+    }
+}
---
+++ src/main/java/com/MyProjects/Server.java
@@ -0,0 +1,14 @@
+package com.MyProjects;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Server extends JFrame {
+
+    public Server(){
+         setSize(450,700);
+         setLocation(200,50);
+         getContentPane().setBackground(Color.WHITE);
+         setVisible(true);
+    }
+}
