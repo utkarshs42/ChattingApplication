8cf76a3c097f258a7f2b32df6605bf06a296e424
Utkarsh Shukla <utkarshs9727@gmail.com>
2025-02-14 00:43:09 +0530

    CreatedPanel

--- src/main/java/com/MyProjects/Main.java
+++ src/main/java/com/MyProjects/Main.java
@@ -3,5 +3,6 @@ package com.MyProjects;
 public class Main {
     public static void main(String[] args) {
           Server s = new Server();
+
     }
 }
--- src/main/java/com/MyProjects/Server.java
+++ src/main/java/com/MyProjects/Server.java
@@ -6,6 +6,18 @@ import java.awt.*;
 public class Server extends JFrame {
 
     public Server(){
+         setLayout(null);
+         JPanel p1 = new JPanel();
+         p1.setBackground(new Color(7, 95, 85));
+         p1.setBounds(0, 0, 450, 70);
+         p1.setLayout(null);
+         add(p1);
+
+         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+         JLabel back = new JLabel(i1);
+         back.setBounds(5, 20, 25, 25);
+         p1.add(back);
+
          setSize(450,700);
          setLocation(200,50);
          getContentPane().setBackground(Color.WHITE);
