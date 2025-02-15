c8e42e3a1a64fdada9c5a8faea6067d84ce15a4f
Utkarsh Shukla <utkarshs9727@gmail.com>
2025-02-15 13:13:37 +0530

    Added message in main class

--- src/main/java/com/MyProjects/Main.java
+++ src/main/java/com/MyProjects/Main.java
@@ -2,6 +2,12 @@ package com.MyProjects;
 
 public class Main {
     public static void main(String[] args) {
-
+         /*
+          This Project is just for understanding Socket and JSwing
+          and all the principals of Designing(literally SOLID is crying in corner :xd)
+          is compromised just to save time.
+          will make a new project with enhanced capability using the knowlege
+          i learned while building this project.
+          */
     }
 }
